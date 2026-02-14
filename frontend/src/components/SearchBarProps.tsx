1cab86b837f2f91f0b1c0c4c47c3dbaa58a08802
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2026-02-14 05:57:14 +0000

    feat: add searchbar component

---
+++ frontend/src/components/SearchBarProps.tsx
@@ -0,0 +1,16 @@
+type SearchBarProps = {
+    value: string
+    onchange: (value: string) => void
+}
+
+const SearchBar = ({ value, onChange }: SearchBarProps) => (
+    <input
+        type="text"
+        className="w-full md:w-1/2 px-4 py-2 border rounded mb-4 focus:outline-none focus:ring-blue-500"
+        placeholder="Search Hackathons..."
+        value={value}
+        onChange={e => onChange(e.target.value)}
+    />
+)
+
+export default SearchBar
