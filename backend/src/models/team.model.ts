36ec4d1e42881011dc28a7251cff79eda520c69e
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2026-02-21 14:07:25 +0000

    feat: add team model

---
+++ backend/src/models/team.model.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+const teamSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    members: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        }
+    ],
+    invites: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+}, { timestamps: true });
+
+export default mongoose.model('Team', teamSchema);
