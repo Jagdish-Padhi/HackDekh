43dfd75064266808edbacac8c059b454dc35d213
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2026-02-22 09:54:33 +0000

    fix: bearer token erro in auth middleware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -5,8 +5,9 @@ import User from "../models/user.model.ts";
 
 export const verifyJWT = asyncHandler(async (req: any, res: any, next: any) => {
     try {
-        const token = req.cookies.accessToken || req.header("authorization")?.replace("Bearer", "");
-
+const token =
+  (req.cookies && req.cookies.accessToken) ||
+  (req.header("authorization") && req.header("authorization").replace("Bearer ", ""));
         if (!token) {
             throw new ApiError(401, "Unauthorized request");
         }
